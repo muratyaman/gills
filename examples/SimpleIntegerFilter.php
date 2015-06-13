19a2c31a10daf34703cefb8ca58200590dfc1553
Haci Murat Yaman <haci@muratyaman.co.uk>
2015-06-13 14:28:18 +0100

    minor update to check integration with packagist

--- examples/SimpleIntegerFilter.php
+++ examples/SimpleIntegerFilter.php
@@ -4,7 +4,7 @@ use Gills\Filters\IntegerFilter;
 
 $intFilter = new IntegerFilter();
 
-//$ageMin = $intFilter->filter('08');//fails with PHP filter
+$ageMin = $intFilter->filter('08');//fails with PHP filter
 
 $ageMin = $intFilter->filter('18');
 
